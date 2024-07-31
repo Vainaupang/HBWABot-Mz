a6134ad9115db1577f6077d2a2e54689be85a5f5
Vainaupang <vaithangteaa@gmail.com>
2024-07-31 19:58:18 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,7 +22,7 @@ global.themeemoji = '🤖'
 global.creator = "916909438834@s.whatsapp.net" // @ symbol hmaa number hi i phone number in thlak rawh
 
 global.ownerNumber = ["916909438834@s.whatsapp.net"]// @ symbol hmaa number hi i phone number in thlak rawh
-global.prefixc = ['/'] // Prefix a tana hman theih loh tur symbolise a awm nuala, i bot a work a lo a nih chuan i thlak mai dawn nia
+global.prefixc = [''] // Prefix a tana hman theih loh tur symbolise a awm nuala, i bot a work a lo a nih chuan i thlak mai dawn nia
 global.sessionName = 'session'
 
 // Sticker Pack Name
